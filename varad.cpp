b41f6335615e062e2b70f76d74db05b276bea777
Varadraj Jagtap <varadraj@Varadrajs-MacBook-Air.local>
2026-02-24 19:17:10 +0530

    Files added

---
+++ varad.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+    class Electrical {
+    public:
+    void trical()
+    {
+        cout<<"This is a circuit based branch";
+
+    } };
+ class extc : public Electrical {
+    public:
+      void tc ()
+      {
+    cout<<"Both hardware software ";
+      }
+ };
+
+
+    
+    int main ()
+    {
+     extc a;
+     a.tc();
+
+
+    
+
+
+
+
+
+    return 0;
+}
